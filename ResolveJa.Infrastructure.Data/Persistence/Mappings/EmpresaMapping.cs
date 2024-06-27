0e507922faf0ce3a929e9545dec0c1765aaaa0e5
eduardocaas <eduardofc.dev@gmail.com>
2024-06-27 19:55:44 -0300

    Implemented interface

--- ResolveJa.Infrastructure.Data/Persistence/Mappings/EmpresaMapping.cs
+++ ResolveJa.Infrastructure.Data/Persistence/Mappings/EmpresaMapping.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ResolveJa.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,11 @@ using System.Threading.Tasks;
 
 namespace ResolveJa.Infrastructure.Data.Persistence.Mappings
 {
-    public class EmpresaMapping
+    public class EmpresaMapping : IEntityTypeConfiguration<Empresa>
     {
+        public void Configure(EntityTypeBuilder<Empresa> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
