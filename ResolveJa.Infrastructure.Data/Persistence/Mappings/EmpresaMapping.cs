02e9b2ea29088f044547846b8a7b65dfe24789a3
eduardocaas <eduardofc.dev@gmail.com>
2024-06-27 19:54:35 -0300

    Add EmpresaMapping.cs

---
+++ ResolveJa.Infrastructure.Data/Persistence/Mappings/EmpresaMapping.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolveJa.Infrastructure.Data.Persistence.Mappings
+{
+    public class EmpresaMapping
+    {
+    }
+}
