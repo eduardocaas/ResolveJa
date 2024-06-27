97e2d89dfbd70b88f46e86d9fda9096ee5f7e34b
eduardocaas <eduardofc.dev@gmail.com>
2024-06-27 18:57:03 -0300

    Add IdentityDbContext

--- ResolveJa.Infrastructure.Data/Persistence/ResolveJaDbContext.cs
+++ ResolveJa.Infrastructure.Data/Persistence/ResolveJaDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace ResolveJa.Infrastructure.Data.Persistence
 {
-    public class ResolveJaDbContext
+    public class ResolveJaDbContext : IdentityDbContext
     {
+        public ResolveJaDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
