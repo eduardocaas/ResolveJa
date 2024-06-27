1683341edd45273ae74d0221b99815d938178c7c
eduardocaas <eduardofc.dev@gmail.com>
2024-06-27 19:06:05 -0300

    Implemented properties

--- ResolveJa.Core/Entities/Empresa.cs
+++ ResolveJa.Core/Entities/Empresa.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace ResolveJa.Core.Entities
 {
-    public class Empresa
+    public class Empresa : BaseEntity
     {
+        public string Url { get; private set; }
+        public string Nome { get; private set; }
+        public string Cnpj { get; private set; }
+        public string Ramo { get; private set; }
+        public string Descricao { get; private set; }
+        public DateTime DataAdmissao { get; private set; }
     }
 }
