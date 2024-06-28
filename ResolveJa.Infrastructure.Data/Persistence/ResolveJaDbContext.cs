9c3b6539943349894ce1be59d05507e00ec1f4ae
eduardocaas <eduardofc.dev@gmail.com>
2024-06-28 19:13:42 -0300

    Implemented DbSets

--- ResolveJa.Infrastructure.Data/Persistence/ResolveJaDbContext.cs
+++ ResolveJa.Infrastructure.Data/Persistence/ResolveJaDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ResolveJa.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +12,16 @@ namespace ResolveJa.Infrastructure.Data.Persistence
 {
     public class ResolveJaDbContext : IdentityDbContext
     {
-        public ResolveJaDbContext(DbContextOptions options) : base(options)
+        public ResolveJaDbContext(DbContextOptions<ResolveJaDbContext> options) : base(options)
         {
-
         }
+
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+            =>  builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        
     }
 }
