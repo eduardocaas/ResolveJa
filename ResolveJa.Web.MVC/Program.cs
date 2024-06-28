c92a6188983a5e5ca8405378ca1b5e9a266b689a
eduardocaas <eduardofc.dev@gmail.com>
2024-06-28 19:07:17 -0300

    Add DbContext

--- ResolveJa.Web.MVC/Program.cs
+++ ResolveJa.Web.MVC/Program.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using ResolveJa.Web.MVC.Data;
+using ResolveJa.Infrastructure.Data.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<ResolveJaDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ResolveJaDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
