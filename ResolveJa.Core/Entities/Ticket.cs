015ea4edb49c6a0007f3860ad3e5cc12605c7864
eduardocaas <eduardofc.dev@gmail.com>
2024-06-28 16:05:12 -0300

    Fix Status field visibility

--- ResolveJa.Core/Entities/Ticket.cs
+++ ResolveJa.Core/Entities/Ticket.cs
@@ -12,7 +12,7 @@ namespace ResolveJa.Core.Entities
         public string Titulo { get; private set; }
         public string Cpf { get; private set; }
         public string Email { get; private set; }
-        public TicketStatusEnum Status { get; private set; }
+        public TicketStatusEnum Status { get; set; }
         public string Conteudo { get; private set; }
         public string Resposta { get; private set; }
         public DateTime DataCriacao { get; private set; }
