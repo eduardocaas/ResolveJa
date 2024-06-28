5760474febb81ec51dc998fea4b92dd925ba2deb
eduardocaas <eduardofc.dev@gmail.com>
2024-06-28 16:07:48 -0300

    Implemented mappings

--- ResolveJa.Infrastructure.Data/Persistence/Mappings/TicketMapping.cs
+++ ResolveJa.Infrastructure.Data/Persistence/Mappings/TicketMapping.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using ResolveJa.Core.Entities;
+using ResolveJa.Core.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,59 @@ using System.Threading.Tasks;
 
 namespace ResolveJa.Infrastructure.Data.Persistence.Mappings
 {
-    public class TicketMapping : IEntityTypeConfiguration<Ticket>
+    public class TicketMapping : IEntityTypeConfiguration<Ticket>       
     {
         public void Configure(EntityTypeBuilder<Ticket> builder)
         {
-            throw new NotImplementedException();
+            builder
+                .ToTable("Ticket")
+                .HasKey(t => t.Id);
+
+            builder
+                .Property(t => t.Titulo)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80)
+                .IsRequired();
+
+            builder
+                .Property(t => t.Cpf)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(11)
+                .IsRequired();
+
+            builder
+                .Property(t => t.Email)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80)
+                .IsRequired();
+
+            builder
+                .Property(t => t.Status)
+                .HasDefaultValue(TicketStatusEnum.ABERTO)
+                .IsRequired(false);
+
+            builder
+                .Property(t => t.Conteudo)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(1000)
+                .IsRequired();
+
+            builder
+                .Property(t => t.Resposta)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(1000)
+                .IsRequired(false);
+
+            builder
+                .Property(t => t.DataCriacao)
+                .HasColumnType("DATETIME")
+                .HasDefaultValueSql("GETDATE()")
+                .IsRequired(false);
+
+            builder
+                .Property(t => t.DataFechamento)
+                .HasColumnType("DATETIME")
+                .IsRequired(false);
         }
     }
 }
