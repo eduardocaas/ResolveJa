a11cba28f3d1400c834697f097cf1a40c2a4f044
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-01 20:25:31 -0300

    Add IEmpresaMvcService

---
+++ ResolveJa.Application/MvcServices/Interfaces/IEmpresaMvcService.cs
@@ -0,0 +1,14 @@
+﻿using ResolveJa.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolveJa.Application.MvcServices.Interfaces
+{
+    public interface IEmpresaMvcService
+    {
+        void CreateEmpresa(EmpresaCreateInputModel model);
+    }
+}
