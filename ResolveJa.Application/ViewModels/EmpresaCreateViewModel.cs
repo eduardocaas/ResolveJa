afe20afb81435a2c4b043b721acafe7f6d87f85d
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-01 19:54:36 -0300

    Add EmpresaCreateViewModel.cs

---
+++ ResolveJa.Application/ViewModels/EmpresaCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ResolveJa.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolveJa.Application.ViewModels
+{
+    public class EmpresaCreateViewModel
+    {
+        public Empresa Empresa { get; set; }
+        [Required]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
+        public string SenhaGestor { get; set; }
+    }
+}
