12fbac6706043345940e54bc1c96f26728d198db
eduardocaas <eduardofc.dev@gmail.com>
2024-07-01 16:11:22 -0300

    Allow only to users in role 'Admin'

--- ResolveJa.Web.MVC/Controllers/EmpresasController.cs
+++ ResolveJa.Web.MVC/Controllers/EmpresasController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using ResolveJa.Infrastructure.Data.Persistence;
 
 namespace ResolveJa.Web.MVC.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class EmpresasController : Controller
     {
         private readonly ResolveJaDbContext _context;
