8eabb4a94cf3dc2ee711a96e6906310eadbb449b
eduardocaas <eduardofc.dev@gmail.com>
2024-07-01 12:44:25 -0300

    Implemented method

--- ResolveJa.Infrastructure.Data/Persistence/ResolveJaDbContextFactory.cs
+++ ResolveJa.Infrastructure.Data/Persistence/ResolveJaDbContextFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace ResolveJa.Infrastructure.Data.Persistence
 {
     public class ResolveJaDbContextFactory
     {
+        public ResolveJaDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ResolveJaDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Trusted_Connection=True;TrustServerCertificate=True;Database=ResolveJa_Test;");
+
+            return new ResolveJaDbContext(optionsBuilder.Options);
+        }
     }
 }
