a777e60ddcaf2e7999d10f7a2a54326f86bda4dc
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-01 20:30:50 -0300

    Add Dependency injection

--- ResolveJa.Application/MvcServices/Implementations/EmpresaMvcServiceImpl.cs
+++ ResolveJa.Application/MvcServices/Implementations/EmpresaMvcServiceImpl.cs
@@ -1,5 +1,7 @@
-﻿using ResolveJa.Application.MvcServices.Interfaces;
+﻿using Microsoft.AspNetCore.Identity;
+using ResolveJa.Application.MvcServices.Interfaces;
 using ResolveJa.Application.ViewModels;
+using ResolveJa.Infrastructure.Data.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,15 @@ namespace ResolveJa.Application.MvcServices.Implementations
 {
     public class EmpresaMvcServiceImpl : IEmpresaMvcService
     {
+        private readonly ResolveJaDbContext _context;
+        public readonly IPasswordHasher<IdentityUser> _passwordHasher;
+
+        public EmpresaMvcServiceImpl(ResolveJaDbContext context, IPasswordHasher<IdentityUser> passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
         public void CreateEmpresa(EmpresaCreateInputModel model)
         {
             throw new NotImplementedException();
