56307b61255c4571d50efdcf2149f894c93e6322
eduardocaas <eduardofc.dev@gmail.com>
2024-07-01 14:26:31 -0300

    Add DbContext

---
+++ ResolveJa.Web.MVC/Data/ResolveJaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ResolveJa.Core.Entities;
+
+namespace ResolveJa.Infrastructure.Data.Persistence
+{
+    public class ResolveJaDbContext : DbContext
+    {
+        public ResolveJaDbContext (DbContextOptions<ResolveJaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ResolveJa.Core.Entities.Funcionario> Funcionario { get; set; } = default!;
+    }
+}
