87ee90c055b5f211f74b8d1db073a62b94950e69
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-01 21:40:49 -0300

    Add Role manager

--- ResolveJa.Application/MvcServices/Implementations/EmpresaMvcServiceImpl.cs
+++ ResolveJa.Application/MvcServices/Implementations/EmpresaMvcServiceImpl.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using ResolveJa.Application.MvcServices.Interfaces;
 using ResolveJa.Application.ViewModels;
 using ResolveJa.Infrastructure.Data.Persistence;
+using ResolveJa.Web.MVC.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +16,13 @@ namespace ResolveJa.Application.MvcServices.Implementations
     {
         private readonly ResolveJaDbContext _context;
         public readonly IPasswordHasher<IdentityUser> _passwordHasher;
+        private readonly IServiceProvider _services;
 
-        public EmpresaMvcServiceImpl(ResolveJaDbContext context, IPasswordHasher<IdentityUser> passwordHasher)
+        public EmpresaMvcServiceImpl(ResolveJaDbContext context, IPasswordHasher<IdentityUser> passwordHasher, IServiceProvider services)
         {
             _context = context;
             _passwordHasher = passwordHasher;
+            _services = services;
         }
 
         public void CreateEmpresa(EmpresaCreateInputModel model)
@@ -28,12 +32,14 @@ namespace ResolveJa.Application.MvcServices.Implementations
             _context.Empresas.Add(model.Empresa);
             _context.SaveChanges();
 
+            
+
             Guid guid = Guid.NewGuid();
             identityGestor.Id = guid.ToString();
             identityGestor.UserName = model.Empresa.Url.ToString();
             identityGestor.Email = (model.Empresa.Url.ToString() + "@email.com");
             identityGestor.NormalizedUserName = (model.Empresa.Url.ToString() + "@email.com");
-
+           
             _context.Users.Add(identityGestor);
 
             var hashedPassword = _passwordHasher.HashPassword(identityGestor, model.SenhaGestor);
@@ -41,6 +47,9 @@ namespace ResolveJa.Application.MvcServices.Implementations
             identityGestor.PasswordHash = hashedPassword;
 
             _context.SaveChanges();
+
+            var userManager = _services.GetRequiredService<UserManager<IdentityUser>>();
+            userManager.AddToRoleAsync(identityGestor, Roles.Gestor);
         }
     }
 }
