c0579c62326c6db7d0e134c2853be7509cdacf4f
eduardocaas <eduardofc.dev@gmail.com>
2024-07-04 15:33:09 -0300

    Add UserMvcService injection

--- ResolveJa.Application/MvcServices/Implementations/EmpresaMvcServiceImpl.cs
+++ ResolveJa.Application/MvcServices/Implementations/EmpresaMvcServiceImpl.cs
@@ -15,16 +15,22 @@ namespace ResolveJa.Application.MvcServices.Implementations
     public class EmpresaMvcServiceImpl : IEmpresaMvcService
     {
         private readonly ResolveJaDbContext _context;
-        public readonly IPasswordHasher<IdentityUser> _passwordHasher;
+        private readonly UserMvcServiceImpl _userMvcService;
+        private readonly IPasswordHasher<IdentityUser> _passwordHasher;
         private readonly IServiceProvider _services;
 
-        public EmpresaMvcServiceImpl(ResolveJaDbContext context, IPasswordHasher<IdentityUser> passwordHasher, IServiceProvider services)
+        public EmpresaMvcServiceImpl(
+            ResolveJaDbContext context, 
+            UserMvcServiceImpl userMvcService,
+            IPasswordHasher<IdentityUser> passwordHasher, 
+            IServiceProvider services)
         {
             _context = context;
+            _userMvcService = userMvcService;
             _passwordHasher = passwordHasher;
             _services = services;
         }
-
+        
         public void CreateEmpresa(EmpresaCreateInputModel model) // Realiza persistência da Empresa e cria usuário padrão
         {
             _context.Empresas.Add(model.Empresa);
