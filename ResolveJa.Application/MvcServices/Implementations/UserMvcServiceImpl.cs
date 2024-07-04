5f1df5169f05b3e995df39ceba9463e4533aa7c0
eduardocaas <eduardofc.dev@gmail.com>
2024-07-04 15:03:54 -0300

    Implemented inteface

--- ResolveJa.Application/MvcServices/Implementations/UserMvcServiceImpl.cs
+++ ResolveJa.Application/MvcServices/Implementations/UserMvcServiceImpl.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ResolveJa.Application.MvcServices.Interfaces;
+using ResolveJa.Application.ViewModels;
 
 namespace ResolveJa.Application.MvcServices.Implementations
 {
-    public class UserMvcServiceImpl
+    public class UserMvcServiceImpl : IUserMvcService
     {
+        public void CreateGestorUser(EmpresaCreateInputModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
