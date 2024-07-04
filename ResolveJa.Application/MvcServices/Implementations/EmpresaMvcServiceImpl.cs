4bfbb9f412b8b25d15a99abff98e1e10274b4573
eduardocaas <eduardofc.dev@gmail.com>
2024-07-04 15:34:48 -0300

    GestorUser creation method call

--- ResolveJa.Application/MvcServices/Implementations/EmpresaMvcServiceImpl.cs
+++ ResolveJa.Application/MvcServices/Implementations/EmpresaMvcServiceImpl.cs
@@ -36,29 +36,7 @@ namespace ResolveJa.Application.MvcServices.Implementations
             _context.Empresas.Add(model.Empresa);
             _context.SaveChanges();
 
-            CreateIdentityUserAndRole(model);
-        }
-
-        public void CreateIdentityUserAndRole(EmpresaCreateInputModel model) // Cria usuário Identity para empresa e aloca na Role Gestor
-        {
-            IdentityUser identityGestor = new IdentityUser();
-
-            Guid guid = Guid.NewGuid();
-            identityGestor.Id = guid.ToString();
-            identityGestor.UserName = model.Empresa.Url.ToString();
-            identityGestor.Email = (model.Empresa.Url.ToString() + "@email.com");
-            identityGestor.NormalizedUserName = (model.Empresa.Url.ToString() + "@email.com");
-
-            _context.Users.Add(identityGestor);
-
-            var hashedPassword = _passwordHasher.HashPassword(identityGestor, model.SenhaGestor);
-            identityGestor.SecurityStamp = Guid.NewGuid().ToString();
-            identityGestor.PasswordHash = hashedPassword;
-
-            _context.SaveChanges();
-
-            var userManager = _services.GetRequiredService<UserManager<IdentityUser>>();
-            userManager.AddToRoleAsync(identityGestor, Roles.Gestor);
+            _userMvcService.CreateGestorUser(model);
         }
     }
 }
