67919e01dcaacdfad7c2fd6798cb3ca39d7ee3d4
eduardocaas <eduardofc.dev@gmail.com>
2024-07-04 15:10:41 -0300

    Implemented method CreateGestorUser

--- ResolveJa.Application/MvcServices/Implementations/UserMvcServiceImpl.cs
+++ ResolveJa.Application/MvcServices/Implementations/UserMvcServiceImpl.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using ResolveJa.Application.MvcServices.Interfaces;
 using ResolveJa.Application.ViewModels;
+using ResolveJa.Web.MVC.Common;
 
 namespace ResolveJa.Application.MvcServices.Implementations
 {
@@ -12,7 +15,24 @@ namespace ResolveJa.Application.MvcServices.Implementations
     {
         public void CreateGestorUser(EmpresaCreateInputModel model)
         {
-            throw new NotImplementedException();
+            IdentityUser identityGestor = new IdentityUser();
+
+            Guid guid = Guid.NewGuid();
+            identityGestor.Id = guid.ToString();
+            identityGestor.UserName = model.Empresa.Url.ToString();
+            identityGestor.Email = (model.Empresa.Url.ToString() + "@email.com");
+            identityGestor.NormalizedUserName = (model.Empresa.Url.ToString() + "@email.com");
+
+            _context.Users.Add(identityGestor);
+
+            var hashedPassword = _passwordHasher.HashPassword(identityGestor, model.SenhaGestor);
+            identityGestor.SecurityStamp = Guid.NewGuid().ToString();
+            identityGestor.PasswordHash = hashedPassword;
+
+            _context.SaveChanges();
+
+            var userManager = _services.GetRequiredService<UserManager<IdentityUser>>();
+            userManager.AddToRoleAsync(identityGestor, Roles.Gestor);
         }
     }
 }
