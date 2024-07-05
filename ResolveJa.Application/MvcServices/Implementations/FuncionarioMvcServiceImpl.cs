43d724a34d19d931fd7a9317a211756394f02bf3
eduardocaas <eduardofc.dev@gmail.com>
2024-07-04 23:02:55 -0300

    Add context DI

--- ResolveJa.Application/MvcServices/Implementations/FuncionarioMvcServiceImpl.cs
+++ ResolveJa.Application/MvcServices/Implementations/FuncionarioMvcServiceImpl.cs
@@ -5,11 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using ResolveJa.Application.MvcServices.Interfaces;
 using ResolveJa.Application.ViewModels;
+using ResolveJa.Infrastructure.Data.Persistence;
 
 namespace ResolveJa.Application.MvcServices.Implementations
 {
     public class FuncionarioMvcServiceImpl : IFuncionarioMvcService
     {
+        private readonly ResolveJaDbContext _context;
+
+        public FuncionarioMvcServiceImpl(ResolveJaDbContext context)
+        {
+            _context = context;
+        }
+
         public void CreateGestor(EmpresaCreateInputModel model)
         {
             throw new NotImplementedException();
