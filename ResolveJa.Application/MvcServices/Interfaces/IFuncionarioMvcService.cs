5d394514977640d704a9fb8c9e62116669ea86a3
eduardocaas <eduardofc.dev@gmail.com>
2024-07-04 22:55:56 -0300

    Created method CreateFuncionarioGestor

--- ResolveJa.Application/MvcServices/Interfaces/IFuncionarioMvcService.cs
+++ ResolveJa.Application/MvcServices/Interfaces/IFuncionarioMvcService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ResolveJa.Application.ViewModels;
 
 namespace ResolveJa.Application.MvcServices.Interfaces
 {
     public interface IFuncionarioMvcService
     {
+        void CreateFuncionarioGestor(EmpresaCreateInputModel model);
     }
 }
