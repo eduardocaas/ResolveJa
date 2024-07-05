52da7cafa49dbeb86ac8b7f8bab9ddd538d34d32
eduardocaas <eduardofc.dev@gmail.com>
2024-07-05 17:55:17 -0300

    Add method GetAll

--- ResolveJa.Application/MvcServices/Interfaces/IFuncionarioMvcService.cs
+++ ResolveJa.Application/MvcServices/Interfaces/IFuncionarioMvcService.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ResolveJa.Application.ViewModels;
+using ResolveJa.Core.Entities;
 
 namespace ResolveJa.Application.MvcServices.Interfaces
 {
     public interface IFuncionarioMvcService
     {
         Task CreateGestor(EmpresaCreateInputModel model);
+        Task<List<Funcionario>> GetAll(string email);
     }
 }
