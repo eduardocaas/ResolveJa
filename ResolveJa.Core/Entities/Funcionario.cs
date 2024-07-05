5f9cef7f86f893894e6bc9a16fbfb2594561fc7a
eduardocaas <eduardofc.dev@gmail.com>
2024-07-05 20:20:53 -0300

    Add type Date for DateTime fields

--- ResolveJa.Core/Entities/Funcionario.cs
+++ ResolveJa.Core/Entities/Funcionario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace ResolveJa.Core.Entities
     {
         public string Email { get; set; }
         public string Nome { get; set; }
+        [DataType(DataType.Date)]
         public DateTime DataAdmissao { get; set; } = DateTime.Now;
 
         public int IdEmpresa { get; set; }
