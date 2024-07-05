0183494f2b6bad3e1d0ba07bd9d30d3d0f335d16
eduardocaas <eduardofc.dev@gmail.com>
2024-07-04 22:58:27 -0300

    Implemented interface

--- ResolveJa.Application/MvcServices/Implementations/FuncionarioMvcServiceImpl.cs
+++ ResolveJa.Application/MvcServices/Implementations/FuncionarioMvcServiceImpl.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ResolveJa.Application.MvcServices.Interfaces;
+using ResolveJa.Application.ViewModels;
 
 namespace ResolveJa.Application.MvcServices.Implementations
 {
-    public class FuncionarioMvcServiceImpl
+    public class FuncionarioMvcServiceImpl : IFuncionarioMvcService
     {
+        public void CreateGestor(EmpresaCreateInputModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
