b75cd4ca31e79941795c4846544b3ff74c411bc4
eduardocaas <eduardofc.dev@gmail.com>
2024-07-06 00:18:41 -0300

    Add method CreateFuncionario

--- ResolveJa.Application/MvcServices/Interfaces/IUserMvcService.cs
+++ ResolveJa.Application/MvcServices/Interfaces/IUserMvcService.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ResolveJa.Application.InputModels;
 
 namespace ResolveJa.Application.MvcServices.Interfaces
 {
     public interface IUserMvcService
     {
         Task CreateGestor(EmpresaCreateInputModel model);
+        Task CreateFuncionario(FuncionarioCreateInputModel model);
     }
 }
