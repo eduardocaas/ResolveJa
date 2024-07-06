05baf584cdebf14a71206dc2088366fe0de51df7
eduardocaas <eduardofc.dev@gmail.com>
2024-07-05 23:19:41 -0300

    Add FuncionarioCreateInputModel

---
+++ ResolveJa.Application/InputModels/FuncionarioCreateInputModel.cs
@@ -0,0 +1,13 @@
+﻿using ResolveJa.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolveJa.Application.InputModels
+{
+    public class FuncionarioCreateInputModel
+    {
+    }
+}
