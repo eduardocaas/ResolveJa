ffb81463f55fa6b1667836ce1639dfe5318d306a
eduardocaas <eduardofc.dev@gmail.com>
2024-07-05 23:20:52 -0300

    Implemented fields

--- ResolveJa.Application/InputModels/FuncionarioCreateInputModel.cs
+++ ResolveJa.Application/InputModels/FuncionarioCreateInputModel.cs
@@ -1,6 +1,7 @@
 ﻿using ResolveJa.Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,10 @@ namespace ResolveJa.Application.InputModels
 {
     public class FuncionarioCreateInputModel
     {
+        public Funcionario Funcionario { get; set; }
+        [Required]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres")]
+        [Display(Name = "Senha Funcionário")]
+        public string SenhaFuncionario { get; set; }
     }
 }
