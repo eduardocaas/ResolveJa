7e242fe35aba397e3230288eed8db3cc42e9f0de
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-08 21:26:55 -0300

    Fix namespace

--- ResolveJa.Core/Entities/Empresa.cs
+++ ResolveJa.Core/Entities/Empresa.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ResolveJa.Application.Validations;
+using ResolveJa.Core.Validations;
 
 namespace ResolveJa.Core.Entities
 {
--- ResolveJa.Core/Validations/CnpjValidation.cs
+++ ResolveJa.Core/Validations/CnpjValidation.cs
@@ -55,4 +55,4 @@ namespace ResolveJa.Core.Validations
         }
     }
 }
-}
+
