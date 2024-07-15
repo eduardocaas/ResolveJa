1e211f18b09e13d22c2e43989ca62107cc3c0479
eduardocaas <eduardofc.dev@gmail.com>
2024-07-15 16:26:56 -0300

    Add constructor and injection

--- ResolveJa.Application/Api/Services/Implementations/EmpresaApiServiceImpl.cs
+++ ResolveJa.Application/Api/Services/Implementations/EmpresaApiServiceImpl.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ResolveJa.Application.Api.Services.Interfaces;
+using ResolveJa.Infrastructure.Data.Persistence;
 
 namespace ResolveJa.Application.Api.Services.Implementations
 {
     public class EmpresaApiServiceImpl : IEmpresaApiService
     {
+        private readonly ResolveJaDbContext _context;
+
+        public EmpresaApiServiceImpl(ResolveJaDbContext context)
+        {
+            _context = context;
+        }
+
         public Task<int> GetId(string url)
         {
             throw new NotImplementedException();
