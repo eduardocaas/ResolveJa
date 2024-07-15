bd924e41d71a9eeef522c3097effcbcc01ee365c
eduardocaas <eduardofc.dev@gmail.com>
2024-07-15 17:42:29 -0300

    Add public access Set

--- ResolveJa.Application/Api/InputModels/TicketCreateInputModel.cs
+++ ResolveJa.Application/Api/InputModels/TicketCreateInputModel.cs
@@ -8,10 +8,10 @@ namespace ResolveJa.Application.Api.InputModels
 {
     public class TicketCreateInputModel
     {
-        public string Titulo { get; private set; }
-        public string Cpf { get; private set; }
-        public string Email { get; private set; }
-        public string Conteudo { get; private set; }
-        public string UrlEmpresa { get; private set; }
+        public string Titulo { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Conteudo { get; set; }
+        public string UrlEmpresa { get; set; }
     }
 }
