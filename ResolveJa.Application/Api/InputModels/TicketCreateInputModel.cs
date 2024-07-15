06f88c5cc38d7d5190e88c68977c861ad8627d86
eduardocaas <eduardofc.dev@gmail.com>
2024-07-14 22:08:02 -0300

    Add properties

--- ResolveJa.Application/Api/InputModels/TicketCreateInputModel.cs
+++ ResolveJa.Application/Api/InputModels/TicketCreateInputModel.cs
@@ -8,5 +8,10 @@ namespace ResolveJa.Application.Api.ApiInputModels
 {
     public class TicketCreateInputModel
     {
+        public string Titulo { get; private set; }
+        public string Cpf { get; private set; }
+        public string Email { get; private set; }
+        public string Conteudo { get; private set; }
+        public string UrlEmpresa { get; private set; }
     }
 }
