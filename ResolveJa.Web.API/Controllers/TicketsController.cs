a5850b1004acbec6b8c7493212a125cda519e2b7
eduardocaas <eduardofc.dev@gmail.com>
2024-07-14 23:18:43 -0300

    Add Create method

--- ResolveJa.Web.API/Controllers/TicketsController.cs
+++ ResolveJa.Web.API/Controllers/TicketsController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ResolveJa.Web.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class TicketsController
+    public class TicketsController : ControllerBase
     {
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
     }
 }
