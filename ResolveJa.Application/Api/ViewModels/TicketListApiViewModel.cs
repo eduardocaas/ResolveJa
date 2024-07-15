2b73b878ea8cec01dd5486acbf41d7cca3c6c65d
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-15 20:15:29 -0300

    Implemented properties

--- ResolveJa.Application/Api/ViewModels/TicketListApiViewModel.cs
+++ ResolveJa.Application/Api/ViewModels/TicketListApiViewModel.cs
@@ -8,5 +8,16 @@ namespace ResolveJa.Application.Api.ViewModels
 {
     public struct TicketListApiViewModel
     {
+        public TicketListApiViewModel(int id, string titulo, int status)
+        {
+            Id = id;
+            Titulo = titulo;
+            Status = status;
+        }
+
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int Status { get; set; }
+
     }
 }
