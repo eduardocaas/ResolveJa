9548dbfd4ae0f3c9018f670cbd9e42b10198fe3c
eduardocaas <eduardofc.dev@gmail.com>
2024-07-15 16:25:03 -0300

    Add injections

--- ResolveJa.Application/Api/Services/Implementations/TicketApiServiceImpl.cs
+++ ResolveJa.Application/Api/Services/Implementations/TicketApiServiceImpl.cs
@@ -6,15 +6,19 @@ using System.Threading.Tasks;
 using ResolveJa.Application.Api.InputModels;
 using ResolveJa.Application.Api.Services.Interfaces;
 using ResolveJa.Core.Entities;
+using ResolveJa.Infrastructure.Data.Persistence;
 
 namespace ResolveJa.Application.Api.Services.Implementations
 {
     public class TicketApiServiceImpl : ITicketApiService
     {
+        private readonly ResolveJaDbContext _context;
+        private readonly IEmpresaApiService _empresaApiService;
+
         public Task Create(TicketCreateInputModel inputModel)
         {
 
-
+            throw new NotImplementedException();
             //Ticket ticket = new Ticket(inputModel.Titulo, inputModel.Cpf, inputModel.Email, inputModel.Conteudo);
         }
     }
