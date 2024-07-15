010b4d1f12b2c0d5e16852a2958d4be33c628361
eduardocaas <eduardofc.dev@gmail.com>
2024-07-15 15:22:39 -0300

    Implemented interface

--- ResolveJa.Application/Api/Services/Implementations/EmpresaApiServiceImpl.cs
+++ ResolveJa.Application/Api/Services/Implementations/EmpresaApiServiceImpl.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ResolveJa.Application.Api.Services.Interfaces;
 
 namespace ResolveJa.Application.Api.Services.Implementations
 {
-    internal class EmpresaApiServiceImpl
+    public class EmpresaApiServiceImpl : IEmpresaApiService
     {
+        public Task<int> GetId(string url)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
