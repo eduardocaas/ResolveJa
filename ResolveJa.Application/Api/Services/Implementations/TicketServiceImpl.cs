a232435bf3be237445a2c49914b7f495058ed6eb
eduardocaas <eduardofc.dev@gmail.com>
2024-07-15 15:04:29 -0300

    Implemented interface

--- ResolveJa.Application/Api/Services/Implementations/TicketServiceImpl.cs
+++ ResolveJa.Application/Api/Services/Implementations/TicketServiceImpl.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ResolveJa.Application.Api.InputModels;
+using ResolveJa.Application.Api.Services.Interfaces;
 
 namespace ResolveJa.Application.Api.Services.Implementations
 {
-    public class TicketServiceImpl
+    public class TicketServiceImpl : ITicketService
     {
+        public Task Create(TicketCreateInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
