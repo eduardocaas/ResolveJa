0df8db6d3a7d9fa113c01089832f8109705f61bc
eduardocaas <eduardofc.dev@gmail.com>
2024-07-14 23:21:06 -0300

    Add ITicketService

---
+++ ResolveJa.Application/Api/Services/Interfaces/ITicketService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResolveJa.Application.Api.InputModels;
+
+namespace ResolveJa.Application.Api.Services.Interfaces
+{
+    public interface ITicketService
+    {
+     
+    }
+}
