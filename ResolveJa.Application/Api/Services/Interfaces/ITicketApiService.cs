dbf6386af965630d4de526f99b5a1d6651155794
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-15 20:16:54 -0300

    Add method GetTickets

--- ResolveJa.Application/Api/Services/Interfaces/ITicketApiService.cs
+++ ResolveJa.Application/Api/Services/Interfaces/ITicketApiService.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ResolveJa.Application.Api.InputModels;
+using ResolveJa.Application.Api.ViewModels;
 
 namespace ResolveJa.Application.Api.Services.Interfaces
 {
     public interface ITicketApiService
     {
         Task<int> Create(TicketCreateInputModel inputModel);
+        Task<List<TicketListApiViewModel>> GetTickets(string cpf, string urlEmpresa);
     }
 }
