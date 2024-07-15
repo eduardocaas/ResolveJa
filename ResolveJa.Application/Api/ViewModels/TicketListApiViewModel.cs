902aa857328fbd08f35d2ce2769c0b8ae4e70aeb
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-15 20:29:55 -0300

    Fix enum field

--- ResolveJa.Application/Api/ViewModels/TicketListApiViewModel.cs
+++ ResolveJa.Application/Api/ViewModels/TicketListApiViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ResolveJa.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace ResolveJa.Application.Api.ViewModels
 {
     public struct TicketListApiViewModel
     {
-        public TicketListApiViewModel(int id, string titulo, int status)
+        public TicketListApiViewModel(int id, string titulo, TicketStatusEnum status)
         {
             Id = id;
             Titulo = titulo;
@@ -17,7 +18,7 @@ namespace ResolveJa.Application.Api.ViewModels
 
         public int Id { get; set; }
         public string Titulo { get; set; }
-        public int Status { get; set; }
+        public TicketStatusEnum Status { get; set; }
 
     }
 }
