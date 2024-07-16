1648c3cb93c5640fdad853d641bc2cb90713eb09
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-15 21:05:35 -0300

    Add Validations

--- ResolveJa.Application/Api/InputModels/TicketCreateInputModel.cs
+++ ResolveJa.Application/Api/InputModels/TicketCreateInputModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ResolveJa.Core.Validations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,21 @@ namespace ResolveJa.Application.Api.InputModels
 {
     public class TicketCreateInputModel
     {
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(80, ErrorMessage = "MaxLength")]
+        [MinLength(3, ErrorMessage = "MinLength")]
         public string Titulo { get; set; }
+        [CpfValidation(errorMessage: "Insira um CPF válido")]
+        [Required(ErrorMessage = "Required")]
         public string Cpf { get; set; }
+        [EmailAddress(ErrorMessage = "Insira um email válido")]
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(80, ErrorMessage = "MaxLength")]
+        [MinLength(10, ErrorMessage = "MinLength")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(1000, ErrorMessage = "MaxLength")]
+        [MinLength(10, ErrorMessage = "MinLength")]
         public string Conteudo { get; set; }
         public string UrlEmpresa { get; set; }
     }
