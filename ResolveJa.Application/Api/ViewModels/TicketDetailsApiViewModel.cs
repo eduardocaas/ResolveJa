c500807eeb314925858ea166191aa961de2db2e8
EDUARDO FALIGURSKI CASTRO <632210083@fspoa.br>
2024-07-15 21:31:03 -0300

    Add TIcketDetailsApiViewModel.cs

---
+++ ResolveJa.Application/Api/ViewModels/TicketDetailsApiViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ResolveJa.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolveJa.Application.Api.ViewModels
+{
+    public struct TicketDetailsApiViewModel
+    {
+        public TicketStatusEnum Status { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Conteudo { get; set; }
+        public string Resposta { get; set; }
+    }
+}
