b2d8f69939b7fe7c53ab58fb55bf9c02bdb00a0e
eduardocaas <eduardofc.dev@gmail.com>
2024-07-20 14:18:16 -0300

    Add TicketMvcServiceImpl.cs

---
+++ ResolveJa.Application/Mvc/Services/Implementations/TicketMvcServiceImpl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolveJa.Application.Mvc.Services.Implementations
+{
+    public class TicketMvcServiceImpl
+    {
+    }
+}
