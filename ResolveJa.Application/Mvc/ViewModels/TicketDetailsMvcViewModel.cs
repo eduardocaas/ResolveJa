25ba13dff0d75b89e9fdc76b2ace7ded9ea96ec7
eduardocaas <eduardofc.dev@gmail.com>
2024-07-20 00:28:15 -0300

    Implemented properties

--- ResolveJa.Application/Mvc/ViewModels/TicketDetailsMvcViewModel.cs
+++ ResolveJa.Application/Mvc/ViewModels/TicketDetailsMvcViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ResolveJa.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace ResolveJa.Application.Mvc.ViewModels
 {
     public class TicketDetailsMvcViewModel
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Email { get; set; }
+        public string Conteudo { get; set; }
+        public string? Resposta { get; set; }
+        public DateTime? DataCriacao { get; set; }
+        public DateTime? DataFechamento { get; set; }
+        public int? IdFuncionario { get; set; }
+        public Funcionario? Funcionario { get; set; }
     }
 }
