08ada242c5543f119e31a2f98ec4502da8422b1f
eduardocaas <eduardofc.dev@gmail.com>
2024-07-20 14:26:52 -0300

    Context injection

--- ResolveJa.Application/Mvc/Services/Implementations/TicketMvcServiceImpl.cs
+++ ResolveJa.Application/Mvc/Services/Implementations/TicketMvcServiceImpl.cs
@@ -5,11 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using ResolveJa.Application.Mvc.Services.Interfaces;
 using ResolveJa.Application.Mvc.ViewModels;
+using ResolveJa.Infrastructure.Data.Persistence;
 
 namespace ResolveJa.Application.Mvc.Services.Implementations
 {
     public class TicketMvcServiceImpl : ITicketMvcService
     {
+        private readonly ResolveJaDbContext _context;
+
+        public TicketMvcServiceImpl(ResolveJaDbContext context)
+        {
+            _context = context;
+        }
+
         public Task<List<TicketListMvcViewModel>> GetAll()
         {
             throw new NotImplementedException();
