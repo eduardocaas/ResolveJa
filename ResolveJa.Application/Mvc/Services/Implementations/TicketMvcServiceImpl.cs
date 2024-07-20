8e0133e68960848206a607db9217be06a46506f1
eduardocaas <eduardofc.dev@gmail.com>
2024-07-20 16:26:20 -0300

    Fix details

--- ResolveJa.Application/Mvc/Services/Implementations/TicketMvcServiceImpl.cs
+++ ResolveJa.Application/Mvc/Services/Implementations/TicketMvcServiceImpl.cs
@@ -21,11 +21,11 @@ namespace ResolveJa.Application.Mvc.Services.Implementations
 
         public async Task<List<TicketListMvcViewModel>> GetAll(int idEmpresa)
         {
-            var tickets = _context.Tickets.Where(t => t.IdEmpresa == idEmpresa).ToListAsync();
+            var tickets = await _context.Tickets.Where(t => t.IdEmpresa == idEmpresa).ToListAsync();
 
             List<TicketListMvcViewModel> viewModels = new List<TicketListMvcViewModel>();
 
-            foreach (var ticket in await tickets)
+            foreach (var ticket in tickets)
             {
                 viewModels.Add(new TicketListMvcViewModel(ticket.Id, ticket.Titulo, ticket.Email, ticket.DataCriacao, ticket.IdFuncionario, ticket.Funcionario));    
             }
