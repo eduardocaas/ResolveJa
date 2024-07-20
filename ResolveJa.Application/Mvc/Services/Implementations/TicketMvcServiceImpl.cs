c916503d24cad954fc1f227817f4255d3a5dd99d
eduardocaas <eduardofc.dev@gmail.com>
2024-07-20 14:18:31 -0300

    Implemented interface

--- ResolveJa.Application/Mvc/Services/Implementations/TicketMvcServiceImpl.cs
+++ ResolveJa.Application/Mvc/Services/Implementations/TicketMvcServiceImpl.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ResolveJa.Application.Mvc.Services.Interfaces;
+using ResolveJa.Application.Mvc.ViewModels;
 
 namespace ResolveJa.Application.Mvc.Services.Implementations
 {
-    public class TicketMvcServiceImpl
+    public class TicketMvcServiceImpl : ITicketMvcService
     {
+        public Task<List<TicketListMvcViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
