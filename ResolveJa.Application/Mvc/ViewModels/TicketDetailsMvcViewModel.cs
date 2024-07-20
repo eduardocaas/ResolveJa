40b771233386145ada99d5a9a95c07eb5fed4619
eduardocaas <eduardofc.dev@gmail.com>
2024-07-20 00:29:42 -0300

    Add constructor

--- ResolveJa.Application/Mvc/ViewModels/TicketDetailsMvcViewModel.cs
+++ ResolveJa.Application/Mvc/ViewModels/TicketDetailsMvcViewModel.cs
@@ -9,6 +9,19 @@ namespace ResolveJa.Application.Mvc.ViewModels
 {
     public class TicketDetailsMvcViewModel
     {
+        public TicketDetailsMvcViewModel(int id, string titulo, string email, string conteudo, string? resposta, DateTime dataCriacao, DateTime? dataFechamento, int? idFuncionario, Funcionario? funcionario)
+        {
+            Id = id;
+            Titulo = titulo;
+            Email = email;
+            Conteudo = conteudo;
+            Resposta = resposta;
+            DataCriacao = dataCriacao;
+            DataFechamento = dataFechamento;
+            IdFuncionario = idFuncionario;
+            Funcionario = funcionario;
+        }
+
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Email { get; set; }
