30b1a1c6a3fa1c82ec5461ac8d124c78bd8be3e0
eduardocaas <eduardofc.dev@gmail.com>
2024-07-21 00:11:30 -0300

    Add TicketAtribuirMvcViewModel

---
+++ ResolveJa.Application/Mvc/ViewModels/TicketAtribuirMvcViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResolveJa.Core.Entities;
+
+namespace ResolveJa.Application.Mvc.ViewModels
+{
+    public class TicketAtribuirMvcViewModel
+    {
+        public List<Funcionario> Funcionarios { get; set; } = new List<Funcionario>();
+        public Ticket Ticket { get; set; }
+    }
+}
