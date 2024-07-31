9058279f56b4b87a982b88acfd18b7663564ede6
eduardocaas <eduardofc.dev@gmail.com>
2024-07-30 23:07:10 -0300

    Configured default route

--- ResolveJa.Web.Angular/src/app/app-routing.module.ts
+++ ResolveJa.Web.Angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
-
+const routes: Routes = [
+  {
+    path: '', component: NavComponent
+  }
+];
 
 @NgModule({
   declarations: [],
