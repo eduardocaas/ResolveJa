d20181aac9e99142061777b171f674a3ccfc8292
eduardocaas <eduardofc.dev@gmail.com>
2024-07-30 22:56:42 -0300

    Add 'nav' component

--- ResolveJa.Web.Angular/src/app/app.module.ts
+++ ResolveJa.Web.Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
