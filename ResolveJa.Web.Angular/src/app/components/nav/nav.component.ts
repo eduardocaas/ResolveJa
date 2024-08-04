29b914aae8f3aad931383ddc0fee8a0ce603007a
eduardocaas <eduardofc.dev@gmail.com>
2024-08-04 00:01:58 -0300

    Add service provider

--- ResolveJa.Web.Angular/src/app/components/nav/nav.component.ts
+++ ResolveJa.Web.Angular/src/app/components/nav/nav.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EmpresaService } from '../../services/empresa.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  providers: [EmpresaService]
 })
 export class NavComponent implements OnInit {
 
