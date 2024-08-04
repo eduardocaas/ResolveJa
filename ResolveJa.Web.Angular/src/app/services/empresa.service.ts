e2c7456212aae6ba9af2458ddb7d41404337e6d3
eduardocaas <eduardofc.dev@gmail.com>
2024-08-03 23:37:12 -0300

    Implemented endpoint 'getByUrl'

--- ResolveJa.Web.Angular/src/app/services/empresa.service.ts
+++ ResolveJa.Web.Angular/src/app/services/empresa.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Empresa } from '../models/empresa.model';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpresaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getByUrl(url: string): Observable<Empresa> {
+    return this.http.get<Empresa>(`${API_CONFIG.baseUrl}/api/Empresas/${url}`);
+  }
 }
