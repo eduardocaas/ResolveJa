13f39541c46e9b03c7474c46782319787de69728
eduardocaas <eduardofc.dev@gmail.com>
2024-08-04 19:56:14 -0300

    Add CorsConfigurationTemplate.cs

--- ResolveJa.Infrastructure.IoC/CorsConfigurationTemplate.cs
+++ ResolveJa.Infrastructure.IoC/CorsConfigurationTemplate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace ResolveJa.Infrastructure.IoC
 {
     public static class CorsConfigurationTemplate
     {
+        public static Action<CorsOptions> ConfigureLocal(string policy)
+        {
+            return options => options.AddPolicy(name: policy,
+                policyBuilder =>
+                {
+                    policyBuilder.WithOrigins("http://LOCALHOST")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+        }
+
+
     }
 }
