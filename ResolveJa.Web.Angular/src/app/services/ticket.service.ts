6bfa7390397e87fe5f25b65356e4e4132c706873
eduardocaas <eduardofc.dev@gmail.com>
2024-08-11 22:38:13 -0300

    Implemented endpoint 'getSearch'

--- ResolveJa.Web.Angular/src/app/services/ticket.service.ts
+++ ResolveJa.Web.Angular/src/app/services/ticket.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Ticket } from '../models/ticket.model';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,7 @@ export class TicketService {
     let params = new HttpParams();
     params.set('cpf', cpf);
     params.set('urlEmpresa', urlEmpresa);
+
+    return this.http.get<Array<Ticket>>(`${API_CONFIG.baseUrl}/api/Tickets`, { params: params });
   }
 }
