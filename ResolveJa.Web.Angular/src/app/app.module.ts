d1b79b7841912edaa2eb554dd121c94b950f6c1b
eduardocaas <eduardofc.dev@gmail.com>
2024-08-12 22:19:47 -0300

    Import BrowserAnimationsModule

--- ResolveJa.Web.Angular/src/app/app.module.ts
+++ ResolveJa.Web.Angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +23,7 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
